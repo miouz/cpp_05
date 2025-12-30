c17669eae41ff94945a55368408bb42f1b62b775
Zhou Mi <mizhouis@gmail.com>
2025-12-30 13:01:25 +0100

    update(ex02_main.cpp): update with robotomy test

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -1,16 +1,28 @@
 #include "Bureaucrat.hpp"
 #include "AForm.hpp"
+#include "RobotomyRequestForm.hpp"
 #include "ShrubberyCreationForm.hpp"
 
 int main()
 {
 	Bureaucrat cafNpc(1,"Francois");
 	Bureaucrat cafNpc2(150,"Patricia");
-	AForm	*cerfa01;
 
-	cerfa01 = new ShrubberyCreationForm("WC");
-	cerfa01->beSigned(cafNpc);
+	//test for ShrubberyCreationForm
+	AForm *cerfa01 = new ShrubberyCreationForm("WC");
+	cafNpc.signForm(*cerfa01);
 	cerfa01->execute(cafNpc);
+
+	//test for RobotomyRequestForm
+	AForm	*cerfa02 = new RobotomyRequestForm("aNewFreshIntern");
+	cafNpc.signForm(*cerfa02);
+	//test for the 50% time succeed
+	cerfa02->execute(cafNpc);
+	cerfa02->execute(cafNpc);
+	cerfa02->execute(cafNpc);
+	cerfa02->execute(cafNpc);
+	cerfa02->execute(cafNpc);
+
 	
 	return (0);
 }
