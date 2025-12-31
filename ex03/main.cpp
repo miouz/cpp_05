c6c58eecb55f55262af0ebfbad4d802dbf33cb5f
Mi Zhou <mzhou@student.42.fr>
2025-12-31 11:03:54 +0100

    fix(main leaks): fix leaks in test

--- ex03/main.cpp
+++ ex03/main.cpp
@@ -12,18 +12,31 @@ int main()
 	AForm* rrf;
 	rrf = someRandomIntern.makeForm("robotomy request", "Bender");
 	std::cout << *rrf;
+	delete rrf;
 
 	//my test
 	try{
 	Intern	aNewFreshIntern;
 	AForm* cerfa0 = aNewFreshIntern.makeForm("shrubbery", "Francois");
 	std::cout << *cerfa0;
+	delete cerfa0;
+
 	AForm* cerfa1 = aNewFreshIntern.makeForm("robotomy request", "Michel");
 	std::cout << *cerfa1;
+	delete cerfa1;
+
 	AForm* cerfa2 = aNewFreshIntern.makeForm("presidential pardon", "Jerome");
 	std::cout << *cerfa2;
+	delete cerfa2;
+
 	AForm* cerfa3 = aNewFreshIntern.makeForm("Shrub", "Jerome");
-	std::cout << *cerfa3;
+	delete cerfa3;
+
+	AForm* cerfa4 = aNewFreshIntern.makeForm("robotomy", "Jerome");
+	delete cerfa4;
+
+	AForm* cerfa5 = aNewFreshIntern.makeForm("presidentialpardon", "Jerome");
+	delete cerfa5;
 	}
 	catch(std::exception& e)
 	{
